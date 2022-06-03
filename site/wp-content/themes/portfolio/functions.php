8fdeb2416a081a9b62893c1ffcdc414ae3b8750e
justinvincent <justin.vincent@student.hepl.be>
2022-06-03 23:33:09 +0200

    deleted the custom post-type, using article and adding link to article with acf

--- site/wp-content/themes/portfolio/functions.php
+++ site/wp-content/themes/portfolio/functions.php
@@ -25,7 +25,6 @@ function portfolio_boot_theme()
     }
 }
 
-
 // Désactiver l'éditeur "Gutenberg" de Wordpress
 add_filter('use_block_editor_for_post', '__return_false');
 
@@ -33,43 +32,11 @@ add_filter('use_block_editor_for_post', '__return_false');
 add_theme_support('post-thumbnails');
 
 
-// custom post-type pour les projets
-register_post_type('project', [
-    'label' => 'Projects',
-    'labels' => [
-        'name' => 'Projects',
-        'singular_name' => 'Project',
-    ],
-    'description' => 'Tous les projects realisé par Justin Vincent',
-    'public' => true,
-    'has_archive' => true,
-    'menu_position' => 7,
-    'menu_icon' => 'dashicons-category',
-    'supports' => ['title', 'editor', 'thumbnail'],
-    'rewrite' => ['slug' => 'projet'],
-]);
-
-// recuperer les projets via une requete wordpress
-function portfolio_get_projets($search = null)
-{
-    // 1. on instancie l'objet WP_Query
-    $projets = new Portfolio_CustomSearchQuery([
-        'post_type' => 'projet',
-        'orderby' => 'date',
-        'order' => 'DESC',
-        's' => strlen($search) ? $search : null,
-    ]);
-
-    // 2. on retourne l'objet WP_Query
-    return $projets;
-}
-
 //// Fonction permettant d'inclure des "partials" dans la vue et d'y injecter des variables "locales" (uniquement disponibles dans le scope de l'inclusion).
 
 function portfolio_include(string $partial, array $variables = [])
 {
     extract($variables);
-
     include(__DIR__ . '/partials/' . $partial . '.php');
 }
 
--- site/wp-content/themes/portfolio/index.php
+++ site/wp-content/themes/portfolio/index.php
@@ -4,13 +4,13 @@
         <section class="layout__projets projets">
             <h2 class="projets__title"><?= 'Mes projets' ?></h2>
             <div class="projets__container">
-                <?php
-                if (($projets = portfolio_get_projets())->have_posts()): while ($projets->have_posts()): $projets->the_post();
-                    portfolio_include('projet');
-                endwhile;
-                else: ?>
-                    <p class="projets__empty"><?="Il n'y a pas encore des projets sur ce site." ?></p>
-                <?php endif; ?>
+                <?php if (have_posts()): ?>
+                    <?php  while (have_posts()): the_post();?>
+                    <?php portfolio_include('post')?>
+                    <?php  endwhile; ?>
+                <?php else: ?>
+                    <p class="projets__message">Il n'y a pas d'articles</p>
+                <?php endif;?>
             </div>
         </section>
     </main>
